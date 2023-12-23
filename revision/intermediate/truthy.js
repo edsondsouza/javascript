d9695ebd2730d8584e143d04ba37f6ad68b97bb5
edson <edsondsouzadev@gmail.com>
2023-12-23 10:11:15 +0530

    truthy and falsy

--- revision/intermediate/truthy.js
+++ revision/intermediate/truthy.js
@@ -1,7 +1,44 @@
 const userEmail = []; // "edson@edson.ai", ""
 
 if (userEmail) {
-  console.log("have email");
+  // console.log("have email");
 } else {
-  console.log("no email");
+  // console.log("no email");
 }
+
+
+// truthy and falsy
+
+// falsy values
+// false, 0, -0, "", BigInt 0n, null, undefined, NaN
+
+// truthy
+// true, " ", [], "0", "false", {}, function(){}
+
+if(userEmail.length === 0) {
+  // console.log("Array is empty");
+}
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0) {
+  // console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 15
+
+// ternary operator
+
+let age = 21
+age >= 21 ? console.log("You can marry") : console.log( "You cannot marry")
+
+
+
+
+
+// console.log(val1)
